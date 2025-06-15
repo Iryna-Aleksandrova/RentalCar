859901427160c5720d731fffc48edb591b6a355b
Iryna-Aleksandrova <ira_aleksandrova@ukr.net>
2025-06-15 13:07:49 +0100

    import Navigation from './Navigation/Navigation.jsx';

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navigation from './navigation/Navigation';
+import Navigation from './Navigation/Navigation.jsx';
 
 const Layout = () => {
   return (
