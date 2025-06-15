0853daf4bf782c3e6f69072d2ea32bfc0e165954
Iryna-Aleksandrova <ira_aleksandrova@ukr.net>
2025-06-15 11:55:02 +0100

    Fix import path with explicit .jsx extension

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navigation from './navigation/Navigation';
+import Navigation from './navigation/Navigation.jsx';
 
 const Layout = () => {
   return (
